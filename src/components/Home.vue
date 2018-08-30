d7b64086947382a86aea218e8e6421c622552e09
Nuttapong Rojanavanich <nuttapong.rj@gmail.com>
2018-08-30 09:28:41 +0700

    Add content template

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -2,6 +2,9 @@
   <div class="main-container">
     <vue2-sidebar class="side-nav-bar" :links="navItems" heading="Menu">
     <h1>{{ msg }}</h1>
+    <div class="content">
+      <p v-for="section in contentData" :key="section.id">{{ section.header }}</p>
+    </div>
   </vue2-sidebar>
   </div>
 </template>
@@ -12,13 +15,55 @@ export default {
   data () {
     return {
       msg: 'Internship at Hokkaido University',
+      contentData: [{
+        id: 0,
+        header: 'Fist Item',
+        content: 'Seicomart Da best',
+        pictures: [
+          'a', 'b', 'c'
+        ]
+      },
+      {
+        id: 1,
+        header: 'Second Item',
+        content: 'セイコーマートへようこそ',
+        pictures: [
+          'a', 'b', 'c'
+        ]
+      }],
       navItems: [{
         label: 'First Item',
         href: '',
         icon: '',
         class: 'item',
         links: ''
+      },
+      {
+        label: 'Second Item',
+        href: '',
+        icon: '',
+        class: 'item',
+        links: ''
+      },
+      {
+        label: 'Third Item',
+        href: '',
+        icon: '',
+        class: 'item',
+        links: ''
+      },
+      {
+        label: 'Forth Item',
+        href: '',
+        icon: '',
+        class: 'item',
+        links: ''
       }]
+    }
+  },
+  methods: {
+    scrollTo (targetSection) {
+
     }
   }
 }
